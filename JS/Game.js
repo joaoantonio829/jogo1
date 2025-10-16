c9c9e53cc589cb78e4a90cf591ed794d0b946aed
joaoantonio829 <joaoantoniocarvalhobrandao@gmail.com>
2025-10-16 17:06:47 -0300

    Add files via upload

--- JS/Game.js
+++ JS/Game.js
@@ -1,142 +1,213 @@
-let largura = window.innerWidth;
-let altura = window.innerHeight;
-let vidas = 1;
-let tempo = 15;
-let pontos = 0;
-let criaMorcegoTempo = 1500;
-let maxMunicao = 6;
-let municaoAtual = 6;
+var altura = 0;
+var largura = 0;
+var vidas = 1;
+var tempo = 15;
+var criaMorcegoTempo = 1500;
+var pontos = 0;
+var intervaloMovimento = {};
 
-let coins = parseInt(localStorage.getItem('userCoins') || 0);
-let highscore = parseInt(localStorage.getItem('highScore') || 0);
-let itemMunicaoDuplaAtivo = localStorage.getItem('item_municao_dupla') === 'true';
+var maxMunicao = 6;
+var municaoAtual = maxMunicao;
 
-if (itemMunicaoDuplaAtivo) {
-    maxMunicao *= 2; 
-    localStorage.setItem('item_municao_dupla', 'false');
-    municaoAtual = maxMunicao;
+var nivel = localStorage.getItem('nivelJogo');
+localStorage.setItem('ponts', 0);
+
+if (nivel === 'normal') {
+    criaMorcegoTempo = 1500;
+} else if (nivel === 'dificil') {
+    criaMorcegoTempo = 1000;
+} else if (nivel === 'chucknorris') {
+    criaMorcegoTempo = 750;
 }
 
-const nivel = localStorage.getItem('nivelJogo');
-if (nivel === 'dificil') criaMorcegoTempo = 1000;
-else if (nivel === 'chucknorris') criaMorcegoTempo = 750;
-
-function ajustarTamanho() {
-    largura = window.innerWidth;
+function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
-}
-window.onresize = ajustarTamanho;
-
-function adicionarCoins(qtd) {
-    coins += qtd;
-    localStorage.setItem('userCoins', coins);
+    largura = window.innerWidth;
 }
 
-function atualizaPainelMunicao() {
-    for (let i = 1; i <= maxMunicao; i++) {
-        const bala = document.getElementById('m' + i);
-        if (bala) bala.style.opacity = (i <= municaoAtual) ? '1' : '0.3';
+ajustaTamanhoPalcoJogo();
+
+function tamanhoAleatorio() {
+    var numeroAleatorio = Math.floor(Math.random() * 3);
+    switch (numeroAleatorio) {
+        case 0: return { classe: "morcego1", arquivo: "1.png" };
+        case 1: return { classe: "morcego2", arquivo: "2.png" };
+        case 2: return { classe: "morcego3", arquivo: "3.png" };
+    }
+}
+
+function ladoAleaorio() {
+    var classe = Math.floor(Math.random() * 2);
+    return classe === 0 ? "ladoA" : "ladoB";
+}
+
+function movimentoZigZag(morcegoElement, tempoDeVida) {
+    let direcaoX = (Math.random() > 0.5) ? 1 : -1;
+    let amplitudeY = 10; 
+    let velocidadeX = 1.75;
+    let contador = 0;
+    
+    const id = morcegoElement.id;
+    let movimento;
+    let timerDesaparece;
+
+    const removerMorcego = (perdeVida = false) => {
+        clearInterval(movimento);
+        clearTimeout(timerDesaparece);
+        
+        if (morcegoElement.parentNode) morcegoElement.remove();
+        delete intervaloMovimento[id];
+
+        if (perdeVida) { 
+            let vidaElement = document.getElementById('v' + vidas);
+            if(vidaElement) vidaElement.src = "IMG/skull.png";
+            vidas++;
+            if (vidas > 3) window.location.href = "gameover.html";
+        }
+    };
+    
+    movimento = setInterval(() => {
+        if (!document.getElementById(id)) {
+            removerMorcego(false);
+            return;
+        }
+        
+        contador += 0.15; 
+        
+        let novaPosicaoY = parseInt(morcegoElement.style.top) + (Math.sin(contador) * amplitudeY);
+        let novaPosicaoX = parseInt(morcegoElement.style.left) + (direcaoX * velocidadeX); 
+
+        morcegoElement.style.top = novaPosicaoY + 'px';
+        morcegoElement.style.left = novaPosicaoX + 'px';
+
+    }, 20);
+
+    intervaloMovimento[id] = movimento;
+
+    timerDesaparece = setTimeout(() => {
+        removerMorcego(false); 
+    }, tempoDeVida); 
+    
+    return removerMorcego;
+}
+
+function posicaoRandomica() {
+    
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    var configMorcego = tamanhoAleatorio();
+
+    var morcego = document.createElement('img');
+    
+    var idMorcego = 'morcego-' + Date.now() + '-' + Math.floor(Math.random() * 100);
+    morcego.id = idMorcego;
+    
+    morcego.src = "IMG/" + configMorcego.arquivo;
+    morcego.style.left = posicaoX + "px";
+    morcego.style.top = posicaoY + "px";
+    morcego.style.position = "absolute";
+    
+    let tamanho;
+    if (configMorcego.classe === 'morcego1') {
+        tamanho = '90px';
+    } else if (configMorcego.classe === 'morcego2') {
+        tamanho = '120px';
+    } else {
+        tamanho = '150px';
+    }
+    morcego.style.width = tamanho;
+    morcego.style.height = tamanho;
+
+    document.body.appendChild(morcego);
+
+    const tempoDeVida = criaMorcegoTempo * 3;
+
+    const removerMorcego = movimentoZigZag(morcego, tempoDeVida);
+
+    morcego.onclick = function() {
+        if (municaoAtual <= 0) {
+            return; 
+        }
+
+        var somDisparo = new Audio();
+        somDisparo.src = 'IMG/disparo.mp3';
+        somDisparo.play(); 
+        
+        municaoAtual--;
+        atualizaPainelMunicao();
+
+        if (this.classList.contains('morcego3')) {
+            pontos += 1;
+        } else if (this.classList.contains('morcego2')) {
+            pontos += 2;
+        } else {
+            pontos += 3;
+        }
+        
+        localStorage.setItem('ponts', pontos);
+        
+        var pontuacaoElement = document.getElementById("pontuacao");
+        if(pontuacaoElement) {
+            pontuacaoElement.innerHTML = pontos;
+        }
+
+        removerMorcego(false);
     }
 }
 
 function recarregar() {
     if (municaoAtual < maxMunicao) {
-        new Audio('IMG/recarregar.mp3').play(); 
+        var somRecarregar = new Audio('IMG/recarregar.mp3'); 
+        somRecarregar.play(); 
+        
         municaoAtual = maxMunicao;
         atualizaPainelMunicao();
     }
 }
 
-function criarMorcego() {
-    const random = Math.floor(Math.random() * 3);
-    const config = (random === 0) ? { classe: "morcego1", arquivo: "1.png", tamanho: '90px', pontos: 3 } :
-                   (random === 1) ? { classe: "morcego2", arquivo: "2.png", tamanho: '120px', pontos: 2 } :
-                                    { classe: "morcego3", arquivo: "3.png", tamanho: '150px', pontos: 1 };
-
-    const morcego = document.createElement('img');
-    const id = 'morcego-' + Date.now();
-    morcego.id = id;
-
-    let x = Math.floor(Math.random() * largura) - 90;
-    let y = Math.floor(Math.random() * altura) - 90;
-    morcego.style.left = (x < 0 ? 0 : x) + "px";
-    morcego.style.top = (y < 0 ? 0 : y) + "px";
-
-    morcego.src = "IMG/" + config.arquivo; 
-    morcego.className = config.classe + " " + (Math.random() > 0.5 ? "ladoA" : "ladoB"); 
-    morcego.style.position = "absolute";
-    morcego.style.width = config.tamanho;
-    morcego.style.height = config.tamanho;
-    document.body.appendChild(morcego);
-
-    let velX = (Math.random() * 2) + 1;
-    let dirX = (Math.random() > 0.5) ? 1 : -1;
-    let dirY = (Math.random() > 0.5) ? 1 : -1;
-
-    const movimento = setInterval(() => {
-        let x = parseInt(morcego.style.left);
-        let y = parseInt(morcego.style.top);
-        
-        let novoX = x + (dirX * velX);
-        let novoY = y + (dirY * 1); 
-
-        if (novoX > largura - 90 || novoX < 0) {
-            
-            const vidaElement = document.getElementById('v' + vidas);
-            if(vidaElement) vidaElement.src = "IMG/skull.png";
-            vidas++;
-            
-            clearInterval(movimento);
-            morcego.remove();
-
-            if (vidas > 3) window.location.href = "gameover.html";
-            return;
+function atualizaPainelMunicao() {
+    for (var i = 1; i <= maxMunicao; i++) {
+        var bala = document.getElementById('m' + i);
+        if (bala) {
+            bala.style.opacity = (i <= municaoAtual) ? '1' : '0.3';
         }
-        
-        if (novoY > altura - 90 || novoY < 0) dirY *= -1;
-
-        morcego.style.left = novoX + 'px';
-        morcego.style.top = novoY + 'px';
-
-    }, 20);
-
-    morcego.onclick = function() {
-        if (municaoAtual <= 0) return; 
-
-        new Audio('IMG/disparo.mp3').play(); 
-        municaoAtual--;
-        atualizaPainelMunicao();
-
-        pontos += config.pontos;
-        adicionarCoins(1);
-
-        localStorage.setItem('ponts', pontos);
-        
-        const pontuacaoElement = document.getElementById("pontuacao");
-        if(pontuacaoElement) pontuacaoElement.innerHTML = pontos;
-
-        clearInterval(movimento);
-        this.remove();
     }
 }
 
-setInterval(criarMorcego, criaMorcegoTempo);
-
-const cronometro = setInterval(() => {
+var cronometro = setInterval(function () {
     tempo -= 1;
     if (tempo < 0) {
         clearInterval(cronometro);
+        for (const id in intervaloMovimento) {
+            clearInterval(intervaloMovimento[id]);
+        }
         
-        const pontuacaoFinal = parseInt(localStorage.getItem('ponts'));
-        if (pontuacaoFinal > highscore) localStorage.setItem('highScore', pontuacaoFinal);
+        var pontuacaoFinal = parseInt(localStorage.getItem('ponts'));
+        var highscore = localStorage.getItem('highScore') ? parseInt(localStorage.getItem('highScore')) : 0;
+        
+        if (pontuacaoFinal > highscore) {
+             localStorage.setItem('highScore', pontuacaoFinal);
+        }
 
         window.location.href = "victory.html";
     } else {
-        const cronometroElement = document.getElementById("cronometro");
-        if (cronometroElement) cronometroElement.innerHTML = tempo;
+        var cronometroElement = document.getElementById("cronometro");
+        if (cronometroElement) {
+             cronometroElement.innerHTML = tempo;
+        }
     }
 }, 1000);
 
-const cronometroInit = document.getElementById('cronometro');
-if(cronometroInit) cronometroInit.innerHTML = tempo;
+var cronometroInit = document.getElementById('cronometro');
+if(cronometroInit) {
+    cronometroInit.innerHTML = tempo;
+}
 atualizaPainelMunicao();
+
+setInterval(function(){
+    posicaoRandomica();
+}, criaMorcegoTempo);
